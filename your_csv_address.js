7ae8438e10fa5bc6b667775f52db8794fb8398e7
Mariana Meireles <marianameireles@protonmail.com>
2023-05-04 02:26:33 +0200

    Update your_csv_address.js

--- your_csv_address.js
+++ your_csv_address.js
@@ -1,6 +1,7 @@
 //If you're hosting your csv on github use the following!
-//change all in caps for your info
-//copy and paste the URL inside the "" don't remove the ""
-//https://cdn.jsdelivr.net/gh/GH_USER_NAME/GH_REPOSITORY@master/FILE_NAME.csv
+//a. Upload your CSV file to your GitHub repository.
+//b. Navigate to the file in your repository and click on it.
+//c. Click on the "Download" button to get the raw content URL.
+//d. Replace the URL in your code with the raw content URL.
 
 export const csv_address = "";
